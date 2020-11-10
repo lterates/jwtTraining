5e12045c761c7871060ef5e0117386ec5d4a0ba6
lterates <9180199@esmad.ipp.pt>
2020-11-10 22:12:59 +0000

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let login = require('./routes/login')
 const utilities = require('./utilities')
 
 const auth = function (req, res, next) {
-    let exceptions = ['/login'];
+    let exceptions = ['/login', '/'];
     if (exceptions.indexOf(req.url) >= 0) {
         next();
     } else {
@@ -35,7 +35,6 @@ const auth = function (req, res, next) {
 }
 
 //USE
-
 app.use(express.json());
 app.use(auth)
 app.use('/students', students);
