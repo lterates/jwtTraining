ccadffbd9b9a9b8a97b915cb02d828771c24ca4d
lterates <9180199@esmad.ipp.pt>
2020-11-10 21:07:07 +0000

    let

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const controller = require ('../controllers/users')
+let controller = require ('../controllers/users')
 
 router.post('/login', function (req, res) {
     controller.login(req, res);
